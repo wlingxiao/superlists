299f2d499b27f667fc597faebaa4739c8967000d
wlingxiao <wlingxiao@outlook.com>
2018-03-19 19:55:30 +0800

    saves items when necessary

--- lists/tests.py
+++ lists/tests.py
@@ -35,6 +35,11 @@ class HomePageTest(TestCase):
         })
         self.assertEqual(response.content.decode(), expected_html)
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_item(self):
