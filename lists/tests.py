057a39f69c107516d23f47e33cf081b65c78d44f
wlingxiao <wlingxiao@outlook.com>
2018-03-19 19:58:48 +0800

    redirect to home page

--- lists/tests.py
+++ lists/tests.py
@@ -29,11 +29,8 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string('home.html', {
-            'new_item_text': 'A new list item'
-        })
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
